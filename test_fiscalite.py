2a33022658942cdbece7ac18c5628e86f8218485
TRKTI <titouan.chnn@gmail.com>
2026-08-04 19:40:41 +0200

    Retirer test_fiscalite.py du repo (fichier d'exercice, pas partie de l'app)

--- test_fiscalite.py
+++
@@ -1,9 +0,0 @@
-duree_placement = 6  # années
-gain = 2000  # € de plus-value
-
-if duree_placement < 5:
-    impot = gain * 0.30  # flat tax 30%
-else:
-    impot = gain * 0.172  # PEA après 5 ans : que les prélèvements sociaux
-
-print("Impôt à payer :", round(impot, 2), "€")
